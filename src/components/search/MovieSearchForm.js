9f37833416fb6ec4a9fd6bf3f218e2d25eded634
Emanuel Alamilla <e.alamilla318@gmail.com>
2022-12-12 16:46:06 -0600

    removed color of sidebar

--- src/components/search/MovieSearchForm.js
+++ src/components/search/MovieSearchForm.js
@@ -1,71 +1,73 @@
-import React, { useState } from 'react'
-import { findMovie } from '../../services/ApiController';
-import  MovieSearch  from './MovieSearch';
-import {Button} from 'react-bootstrap';
-
+import React, { useState } from "react";
+import { findMovie } from "../../services/ApiController";
+import MovieSearch from "./MovieSearch";
+import { Button} from "react-bootstrap";
 
 export const MovieSearchForm = () => {
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("");
 
-    const [movies, setMovies] = useState([]);
-    const [query, setQuery] = useState('');
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setQuery(e.target.value);
+    e.target.value = "";
+  };
 
-    const changeHandler = (e) => {
-        e.preventDefault()
-        setQuery(e.target.value)
-        e.target.value = ''
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log("Searching");
+    try {
+      let response = await findMovie(query);
+      response = response.results;
+      setMovies(response);
+      e.target.value = "";
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    const searchMovie = async (e) => {
-        e.preventDefault();
-        console.log("Searching");
-        try {
-            let response = await findMovie(query)
-            response = response.results
-            setMovies(response)
-            e.target.value = ''
-
-        }
-        catch (e) {
-            console.log(e);
-        }
-    }
-
-    return (
-        <>
-
-            <div className="search">
-                <h3 style={{textAlign:"center"}} className="title">Search: {query}</h3>
-                <form onSubmit={searchMovie}>
-                    <input type="text"
-                        id="search_field"
-                        name="movieSearch"
-                        autoComplete='off'
-                        placeholder="Search for a movie"
-                        value={query}
-                        onChange={changeHandler}
-                    />
-                    <div style={{marginTop:"30px"}}>
-                    <Button style={{display:"block",width:"100%"}} size="lg" variant="primary" type="submit" id="search">Search</Button>
-                    </div>
-                </form>
+  return (
+    <>
+        <div className="search">
+          <h3 style={{ textAlign: "center" }} className="title">
+            Search: {query}
+          </h3>
+          <form onSubmit={searchMovie}>
+            <input
+              type="text"
+              id="search_field"
+              name="movieSearch"
+              autoComplete="off"
+              placeholder="Search for a movie"
+              value={query}
+              onChange={changeHandler}
+            />
+            <div style={{ marginTop: "30px",marginBottom:"30px" }}>
+              <Button
+                style={{ display: "block", width: "100%" }}
+                size="lg"
+                variant="primary"
+                type="submit"
+                id="search"
+              >
+                Search
+              </Button>
             </div>
-            <div>
-                {movies.length > 0 ? (
-                    <div className="container">
-                        <div className="grid">
-                            {movies.map((movieReq) =>
-                                <MovieSearch key={movieReq.id} peli={movieReq} />)}
-                        </div>
-                    </div>
-                ) : (
-                    <h2>Sorry !! No Movies Found</h2>
-                )}
+          </form>
+        </div>
+        <div>
+          {movies.length > 0 ? (
+            <div className="container">
+              <div className="grid">
+                {movies.map((movieReq) => (
+                  <MovieSearch key={movieReq.id} peli={movieReq} />
+                ))}
+              </div>
             </div>
-        </>
-
-
-
-
-
-    )
-}
+          ) : (
+            <h2>Sorry !! No Movies Found</h2>
+          )}
+        </div>
+    </>
+  );
+};
